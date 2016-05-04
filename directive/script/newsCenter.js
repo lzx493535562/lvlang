6c40311f0cb28ca462f304473e8ccd84b288a66e
wangcongcong <740707701@qq.com>
2016-05-04 17:48:14 +0800

    ===undefined

--- directive/script/newsCenter.js
+++ directive/script/newsCenter.js
@@ -66,7 +66,7 @@ define(['app',
 				$scope.newsId = $routeParams.newsId;
 
 				$scope.newsDetail = function(){
-					if($scope.newsId=='undefined'){return;};
+					if($scope.newsId===undefined){return;};
 					newsService.newsDetail($scope.newsId)
 					.success(function(data){
 						console.log("detail",data);
